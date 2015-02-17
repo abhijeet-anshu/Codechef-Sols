b2de7d29e41ce53f145c2f90a34c40be4b4a5454
abhijeet baranwal <abhijeet.baranwal@wisecells.com>
2015-02-17 08:44:57 +0000

    removing spoj from public repo

--- spoj.cpp/BISHOPS.py
+++
@@ -1,4 +0,0 @@
-while True:
-	try: num =  int(raw_input())
-	except: break
-	print num*2-2 if num is not 1 else 1
--- spoj.cpp/NGM.cpp
+++
@@ -1,7 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main(void) {
-	int n;scanf("%d", &n);
-	if(n%10==0) {printf("2\n");}
-	else {printf("1\n%d\n", n%10);}
-}
